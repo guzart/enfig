c5666ad3fde157c2487be5a7d27768928314c4dc
Arturo Guzman <arturo@kisurfer.com>
2013-08-05 16:11:08 -0400

    update test files

--- lib/enfig.rb
+++ lib/enfig.rb
@@ -2,18 +2,20 @@ require 'yaml'
 
 class Enfig
 
-  attr_reader :config
   attr_accessor :env, :root, :files, :enable_overwrite
 
   alias_method :'overwrite?', :enable_overwrite
 
   def initialize(args = {})
-    @config           = {}
-    @env              = args[:env]  || ENV['RAILS_ENV'] || ENV['ENV'] || 'development'
+    @config           = nil
+    @env              = args[:env]  || 'development'
     @root             = args[:root] || '.'
     @files            = args[:files] || [args[:file]].compact
     @enable_overwrite = args[:overwrite]
-    add_rails_files if args[:rails]
+  end
+
+  def config
+    @config ||= load_config
   end
 
   def [](key)
@@ -21,37 +23,41 @@ class Enfig
   end
 
   def load_config
-    @config = {}
+    conf = {}
 
     files.each do |file|
       name = File.basename(file, '.yml').to_s.downcase.to_sym
-      @config[name] = load_yaml(file)
+      conf[name] = load_yaml(file)
     end
 
-    @config
+    conf
+  end
+
+  def update_env!
+    load_env_hash(config).each do |k, v|
+      ENV[k] = v
+    end
   end
 
   private
-
-    def add_rails_files
-      @files << [File.join('config', 'project.yml'), File.join('config', 'database.yml')]
-    end
 
     def load_yaml(*args)
       config = YAML.load_file(File.join(root, *args))
       symbolize_keys(config[env])
     end
 
-    def fill_env_hash(config_src, target = {}, prefix = nil)
+    def load_env_hash(config_src, target = {}, prefix = nil)
       prefix.concat('_') if prefix && !prefix.end_with?('_')
       config_src.each do |key, value|
         suffix = key.to_s.upcase
+        target_key = "#{prefix}#{suffix}"
         if value.is_a?(Hash)
-          fill_env_hash(value, target, suffix)
+          load_env_hash(value, target, target_key)
         else
-          target["#{prefix}#{suffix}"] = value
+          target[target_key] = value
         end
       end
+
       target
     end
 
@@ -59,10 +65,10 @@ class Enfig
       transform_keys(hash) { |key| key.to_sym rescue key }
     end
 
-    def transform_keys(hash)
+    def transform_keys(hash, &block)
       result = {}
-      hash.each_key do |key|
-        result[yield(key)] = hash[key]
+      hash.each do |key, value|
+        result[yield(key)] = value.is_a?(Hash) ? transform_keys(value, &block) : value
       end
       result
     end
--- test/test_enfig.rb
+++ test/test_enfig.rb
@@ -3,10 +3,37 @@ require 'enfig'
 
 class EnfigTest < Test::Unit::TestCase
 
-  def test_load_config_by_environment
-    enfig = Enfig.new(:env => 'development', :file => 'test/config.yml')
-    enfig.load_config
-    assert_equal 'Hola', enfig[:config][:hello]
+  def test_load_file_into_key_with_file_basename
+    enfig = Enfig.new(:file => 'test/sample.yml')
+    assert_not_nil enfig[:sample]
+  end
+
+  def test_load_file_using_development_environment_by_default
+    enfig = Enfig.new(:file => 'test/sample.yml')
+    assert_equal 'Hola', enfig[:sample][:hello]
+  end
+
+  def test_load_file_by_specific_environment
+    enfig = Enfig.new(:env => 'production', :file => 'test/sample.yml')
+    assert_equal 'Bonjour', enfig[:sample][:hello]
+  end
+
+  def test_load_file_from_nested_config
+    enfig = Enfig.new(:file => 'test/sample.yml')
+    assert_equal 'abcdefg12345', enfig[:sample][:s3][:key]
+  end
+
+  def test_update_env_variables
+    enfig = Enfig.new(:file => 'test/sample.yml')
+    assert_nil ENV['SAMPLE_HELLO']
+    enfig.update_env!
+    assert_equal 'Hola', ENV['SAMPLE_HELLO']
+  end
+
+  def test_updated_env_variables_from_nested_config
+    enfig = Enfig.new(:file => 'test/sample.yml')
+    enfig.update_env!
+    assert_equal 'abcdefg12345', ENV['SAMPLE_S3_KEY']
   end
 
 end
