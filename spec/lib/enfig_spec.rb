fd3cb89d97e10c45c0483ce7753e7683035a37f8
Arturo Guzman <arturo@kisurfer.com>
2016-01-12 14:34:20 -0500

    chore(specs): sort specs by dependencies

--- spec/lib/enfig_spec.rb
+++ spec/lib/enfig_spec.rb
@@ -6,12 +6,55 @@ require 'byebug'
 describe Enfig do
   let(:filepath) { File.expand_path(File.join('..', 'sample.yml'), File.dirname(__FILE__)) }
 
-  describe '::load!' do
-    it 'use the RAILS_ENV environment value as the default root' do
+  describe '::symbolize_keys' do
+    it 'symbolizes the keys of a Hash' do
+      config = Enfig.symbolize_keys('hello': 'world')
+      expect(config[:hello]).to eq 'world'
+    end
+  end
+
+  describe '#set_env' do
+    it 'sets the config Hash to the ENV (environment variables) using upper case keys' do
       ENV['HELLO'] = nil
-      ENV['RAILS_ENV'] = 'my_env'
-      Enfig.load!(filepath)
-      expect(ENV['HELLO']).to eq 'Guten Morgen'
+      Enfig.set_env(hello: 'World')
+      expect(ENV['HELLO']).to eq 'World'
+    end
+
+    it 'overwrites the environment value if one is already set' do
+      ENV['HELLO'] = 'my value'
+      Enfig.set_env({ hello: 'World' }, overwrite: true)
+      expect(ENV['HELLO']).to eq 'World'
+    end
+
+    it 'overwrites the environment value if one is already set' do
+      ENV['HELLO'] = 'my value'
+      Enfig.set_env({ hello: 'World' }, overwrite: false)
+      expect(ENV['HELLO']).to eq 'my value'
+    end
+  end
+
+  describe '::merge_config!' do
+    it 'merges to hashes with a given key prefix and a separator' do
+      target = { hello: 'world' }
+      Enfig.merge_config!(target, { foo: 'bar' }, :test_)
+      expect(target['test_foo']).to eq 'bar'
+    end
+  end
+
+  describe '::flatten_config' do
+    it 'returns nil if the given config is nil' do
+      config = Enfig.flatten_config(nil)
+      expect(config).to be_nil
+    end
+
+    it 'copies first level keys and values' do
+      config = Enfig.flatten_config(name: 'My Enfig')
+      expect(config[:name]).to eq 'My Enfig'
+    end
+
+    it 'flattens the nested hashes using the given separator' do
+      config = Enfig.flatten_config({ database: { username: 'enfig' } }, '/')
+      expect(config['database/username']).to eq 'enfig'
     end
   end
 
@@ -37,55 +80,12 @@ describe Enfig do
     end
   end
 
-  describe '#set_env' do
-    it 'sets the config Hash to the ENV (environment variables) using upper case keys' do
+  describe '::load!' do
+    it 'use the RAILS_ENV environment value as the default root' do
       ENV['HELLO'] = nil
-      Enfig.set_env(hello: 'World')
-      expect(ENV['HELLO']).to eq 'World'
-    end
-
-    it 'overwrites the environment value if one is already set' do
-      ENV['HELLO'] = 'my value'
-      Enfig.set_env({ hello: 'World' }, overwrite: true)
-      expect(ENV['HELLO']).to eq 'World'
-    end
-
-    it 'overwrites the environment value if one is already set' do
-      ENV['HELLO'] = 'my value'
-      Enfig.set_env({ hello: 'World' }, overwrite: false)
-      expect(ENV['HELLO']).to eq 'my value'
-    end
-  end
-
-  describe '::flatten_config' do
-    it 'returns nil if the given config is nil' do
-      config = Enfig.flatten_config(nil)
-      expect(config).to be_nil
-    end
-
-    it 'copies first level keys and values' do
-      config = Enfig.flatten_config(name: 'My Enfig')
-      expect(config[:name]).to eq 'My Enfig'
-    end
-
-    it 'flattens the nested hashes using the given separator' do
-      config = Enfig.flatten_config({ database: { username: 'enfig' } }, '/')
-      expect(config['database/username']).to eq 'enfig'
-    end
-  end
-
-  describe '::merge_config!' do
-    it 'merges to hashes with a given key prefix and a separator' do
-      target = { hello: 'world' }
-      Enfig.merge_config!(target, { foo: 'bar' }, :test_)
-      expect(target['test_foo']).to eq 'bar'
-    end
-  end
-
-  describe '::symbolize_keys' do
-    it 'symbolizes the keys of a Hash' do
-      config = Enfig.symbolize_keys('hello': 'world')
-      expect(config[:hello]).to eq 'world'
+      ENV['RAILS_ENV'] = 'my_env'
+      Enfig.load!(filepath)
+      expect(ENV['HELLO']).to eq 'Guten Morgen'
     end
   end
 end
