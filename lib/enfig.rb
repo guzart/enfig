f8de1ae31e9ac2596a4bd6b72ff5241d31b544ea
Arturo Guzman <arturo@kisurfer.com>
2016-01-12 01:34:38 -0500

    feat(enfig): refactor main fail to use static functions

--- lib/enfig.rb
+++ lib/enfig.rb
@@ -1,83 +1,84 @@
-require 'yaml'
+require 'psych'
 
 class Enfig
+  DEFAULTS = { overwrite: false, root: nil, separator: '_' }
 
-  attr_accessor :env, :root, :files, :enable_overwrite
+  def self.load_config(filename, options = {})
+    opts = DEFAULTS.merge(symbolize_keys(options))
+    root = opts[:root]
+    separator = opts[:separator]
 
-  alias_method :'overwrite?', :enable_overwrite
-
-  def initialize(args = {})
-    @config           = nil
-    @env              = args[:env]
-    @root             = args[:root]       || ''
-    @files            = args[:files]      || [args[:file]].compact
-    @enable_overwrite = args[:overwrite] == false ? false : true
+    data = Psych.load_file(filename)
+    data = data[root.to_s] unless root.to_s.strip == ''
+    @config = Enfig.symbolize_keys(Enfig.flatten_config(data, separator))
   end
 
-  def config
-    @config ||= load_config
-  end
+  def self.set_env(config, options = {})
+    opts = DEFAULTS.merge(symbolize_keys(options))
+    overwrite = opts[:overwrite]
 
-  def [](key)
-    config[key]
-  end
-
-  def load_config
-    conf = {}
-
-    files.each do |file|
-      name = File.basename(file, '.yml').to_s.downcase.gsub(/[^a-z0-9_]/i, '_')
-      conf[name.to_sym] = load_yaml(file)
-    end
-
-    conf
-  end
-
-  def update_env!
-    load_env_hash(config).each do |k, v|
-      ENV[k] = v.to_s if ENV[k].nil? || ENV[k] == '' || overwrite?
+    config.each do |key, value|
+      env_key = key.to_s.upcase
+      next if !overwrite && !ENV[env_key].nil?
+      ENV[env_key] = value
     end
   end
 
-  def self.update!(args = {})
-    enfig = Enfig.new(args)
-    enfig.update_env!
-    enfig
-  end
+  # Flattens the deeply nested hashes into a single hash by joining the keys using the given separator
+  # @param config [Hash] the hash to flatten
+  # @param separator [String|Symbol] the key separator to use when joining the keys of a nested hash
+  # @return [Hash] the flattened hash
+  def self.flatten_config(config, separator = '_')
+    return nil if config.nil?
 
-  private
-
-    def load_yaml(*args)
-      config = YAML.load_file(File.join(root, *args))
-      data   = env.nil? ? config : config[env]
-      symbolize_keys(data)
-    end
-
-    def load_env_hash(config_src, target = {}, prefix = nil)
-      prefix.concat('_') if prefix && !prefix.end_with?('_')
-      config_src.each do |key, value|
-        suffix = key.to_s.upcase
-        target_key = "#{prefix}#{suffix}"
-        if value.is_a?(Hash)
-          load_env_hash(value, target, target_key)
-        else
-          target[target_key] = value
-        end
+    output = {}
+    config.each do |key, value|
+      if value.is_a? Hash
+        flat_value = flatten_config(value, separator)
+        merge_config!(output, flat_value, [key.to_s, separator.to_s].join)
+        next
       end
 
-      target
+      output[key] =  value
     end
 
-    def symbolize_keys(hash)
-      transform_keys(hash) { |key| key.to_sym rescue key }
+    output
+  end
+
+  # Meges a source hash into a target hash by adding a prefix to the source hash keys.
+  # @param target [Hash] the target hash that will be modified
+  # @param source [Hash] the source hash that will be merge into the target
+  # @param key_prefix [Symbol|String] the prefix for the keys in the target
+  # @return [Hash] the target hash
+  def self.merge_config!(target, source, key_prefix)
+    source.each do |key, value|
+      o_key = [key_prefix.to_s, key.to_s].join
+      target[o_key] = value
     end
 
-    def transform_keys(hash, &block)
-      result = {}
-      hash.each do |key, value|
-        result[yield(key)] = value.is_a?(Hash) ? transform_keys(value, &block) : value
-      end
-      result
+    target
+  end
+
+  # Symbolizes the first level keys of the given hash
+  # @param hash [Hash] the source hash
+  # @return [Hash] the hash with symbolized keys
+  def self.symbolize_keys(hash)
+    return nil if hash.nil?
+    output = {}
+    hash.each do |key, value|
+      output[key.to_sym] = value
     end
 
+    output
+  end
+
+  def self.load!(filename, options = {})
+    rails_env = ENV['RAILS_ENV'].to_s.strip
+    rails_env = nil if rails_env == ''
+    args = { root: rails_env }.merge(options)
+
+    config = load_config(filename, args)
+    set_env(config, args)
+    config
+  end
 end
